cf79ac14fbe26d0ae0919467b539ee316d699504
glory <reallynotme.only@gmail.com>
2025-12-02 17:17:26 +0800

    can2usb 2channel

--- src/can_node.cpp
+++ src/can_node.cpp
@@ -12,25 +12,25 @@ void signal_callback_handler(int signum){
 */
 int main()
 {
-         /* set real-time process */
-        pid_t pid = getpid();
-        sched_param param;
-         param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-         if (sched_setscheduler(pid, SCHED_FIFO, &param) == -1) {
+    /* set real-time process */
+    //创建主线程，设置调度策略FIFO，分配最大优先级
+    pid_t pid = getpid();
+    sched_param param;
+    param.sched_priority = sched_get_priority_max(SCHED_FIFO);
+    if (sched_setscheduler(pid, SCHED_FIFO, &param) == -1) {
         std::cout << "[ERROR] Function setProcessScheduler failed." << std::endl;
     }
 
-   //创建对象，调用构造函数
-   //开启订阅，设置回调函数，创建线程，在线程接收LCM数据
-   CAN_ptr = std::make_shared< Tangair_usb2can >();
+    //创建对象，调用构造函数
+    //开启订阅，设置回调函数，创建线程，在线程接收LCM数据
+    CAN_ptr = std::make_shared< Tangair_usb2can >();
 
-//   //设置信号中断
+    //设置信号中断
     signal( SIGINT,  signal_callback_handler);
 
-   //主函数循环，1s延时
+    //主函数循环，1s延时
     CAN_ptr->Spin();
 
-//    
     return 0;
 }
 
